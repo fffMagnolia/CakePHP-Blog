cffc4d2f44f3689367a0268d04f3ace25ac67170
Magnolia <magnolia1925pa@gmail.com>
2019-06-22 18:37:28 +0900

    Changes to be committed: 	modified:   tests/TestCase/Controller/ArticlesControllerTest.php ・Deleteの正常系以外のテスト（最低限）を追加

--- tests/TestCase/Controller/ArticlesControllerTest.php
+++ tests/TestCase/Controller/ArticlesControllerTest.php
@@ -47,10 +47,21 @@ class ArticlesControllerTest extends TestCase
     }
 
     /**
-     * Test add method
+     * 認証を通過しなかった場合リダイレクトされること
      */
-    public function testAdd()
-    {
+    public function testAddUnAuthenticatedFails(){
+        $this->get('/articles/add');
+        //リダイレクトした場合は末尾にクエリがつくので一部をチェック
+        $this->assertRedirectContains('/users/login');
+    }
+
+    /**
+     * 認証を通過した場合のみアクセスできること→OK!
+     * 認証が通過しなかった場合リダイレクトすること->OK!
+     * 記事を追加できること
+     * 不正な形式の記事が追加できないこと
+     */
+    public function testAddAuthenticated() {
         //認証作業
         $user_name = env('USER_NAME');
         $password = env('PASSWORD');
@@ -60,26 +71,59 @@ class ArticlesControllerTest extends TestCase
         
         $this->get('/articles/add/');
         //無事アクセスできたらOK
-        $this->assertResponseOK();
+        $this->assertResponseOk();
+    }
+
+    public function testEditUnAuthenticatedFails() {
+        //2019.6.22現在一番古い記事を参照している
+        $this->get('/articles/edit/3');
+        //ログインページにリダイレクトしたらOK
+        $this->assertRedirectContains('/users/login');
     }
 
     /**
-     * Test edit method
-     *
-     * @return void
+     * 認証が通った場合アクセスできること→OK!
+     * 認証に失敗した場合リダイレクトすること->OK!
+     * 記事の編集後保存できること
+     * 不正な形式の記事を保存できないこと
      */
-    public function testEdit()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
+    public function testEditAuthenticated () {
+        //認証作業
+        $user_name = env('USER_NAME');
+        $password = env('PASSWORD');
+        $this->session([
+            'Auth' => ['User' => ['id' => 1, 'username' => "{$user_name}", 'password' => "{$password}"] ]
+        ]);
+
+        //2019.6.22現在一番古い記事を参照している
+        $this->get('/articles/edit/3');
+        $this->assertResponseOk();
+    }
+
+    public function testDeleteUnAuthenticatedFails() {
+        //2019.6.22現在一番新しい記事を参照している
+        $this->get('/articles/delete/12');
+        //リダイレクトしたらOK
+        $this->assertRedirectContains('/users/login');
     }
 
     /**
-     * Test delete method
-     *
-     * @return void
+     * 認証が通過した場合ダイアログが表示されること
+     * ダイアログ表示後OK押下で記事が削除できること
+     * 認証で通過しなかった場合リダイレクトされること→OK!
+     * ダイアログでCancel押下時に記事が削除されないこと
      */
     public function testDelete()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        //認証作業
+        $user_name = env('USER_NAME');
+        $password = env('PASSWORD');
+        $this->session([
+            'Auth' => ['User' => ['id' => 1, 'username' => "{$user_name}", 'password' => "{$password}"] ]
+        ]);
+
+        //TODO:ここでダイアログがでることを確認したい
+        
+        $this->assertResponseSuccess();
     }
 }
