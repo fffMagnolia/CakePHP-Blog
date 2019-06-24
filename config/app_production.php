2ef0d9a4849c191cbe9819fd90383349dfae0b18
Magnolia <magnolia1925pa@gmail.com>
2019-06-24 16:23:52 +0900

    Appのタイムゾーンの設定を追加

--- config/app_production.php
+++ config/app_production.php
@@ -1,11 +1,35 @@
 <?php
 return [
+    //デバックモードをオフに
     'debug' => false,
 
     'Security' => [
         'salt' => env('SALT'),
     ],
 
+    //基本設定
+    'App' => [
+        'namespace' => 'App',
+        'encoding' => env('APP_ENCODING', 'UTF-8'),
+        'defaultLocale' => env('APP_DEFAULT_LOCALE', 'ja_JP'),
+        'defaultTimezone' => env('APP_DEFAULT_TIMEZONE', 'Asia/Tokyo'),
+        'base' => false,
+        'dir' => 'src',
+        'webroot' => 'webroot',
+        'wwwRoot' => WWW_ROOT,
+        //'baseUrl' => env('SCRIPT_NAME'),
+        'fullBaseUrl' => false,
+        'imageBaseUrl' => 'img/',
+        'cssBaseUrl' => 'css/',
+        'jsBaseUrl' => 'js/',
+        'paths' => [
+            'plugins' => [ROOT . DS . 'plugins' . DS],
+            'templates' => [APP . 'Template' . DS],
+            'locales' => [APP . 'Locale' . DS],
+        ],
+    ],
+
+    //DB設定
     $url = parse_url(env('DATABASE_URL')),
     'Datasources' => [
             'default' => [
